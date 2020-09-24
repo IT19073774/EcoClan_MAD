4ad73009aea9862c689e3c1169bc777ae24ccbf4
IT19073774 <IT19073774@my.sliit.lk>
2020-09-25 03:02:12 +0530

    Authenticated User Login to Recycler UI Commit

--- app/src/main/java/com/example/ecoclan_v2/LogInActivity.java
+++ app/src/main/java/com/example/ecoclan_v2/LogInActivity.java
@@ -16,11 +16,20 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LogInActivity extends AppCompatActivity {
 
     EditText e1_email, e2_psw;
+    String session_Type;
     FirebaseAuth auth;
+    FirebaseFirestore db;
     ProgressDialog dialog;
 
     @Override
@@ -29,7 +38,7 @@ public class LogInActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_log_in);
-
+        db = FirebaseFirestore.getInstance();
         e1_email = findViewById(R.id.email);
         e2_psw = findViewById(R.id.psw);
         auth = FirebaseAuth.getInstance();
@@ -49,10 +58,33 @@ public class LogInActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             dialog.hide();
                             if(task.isSuccessful()){
-                                Toast.makeText(getApplicationContext(),"User Successfully SignedIn", Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(LogInActivity.this,HomeActivity.class);
-                                startActivity(i);
-                                finish();
+
+                                CollectionReference users = db.collection("Users");
+                                Query query = users.whereEqualTo("Email", e1_email.getText().toString());
+                                query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                                        if (e !=null) {}
+                                        for (DocumentChange documentChange : documentSnapshots.getDocumentChanges())
+                                        {
+                                            String type =  documentChange.getDocument().getData().get("Type").toString();
+                                            if (type.equals("Collector")) {
+                                                Toast.makeText(getApplicationContext(), "User Successfully SignedIn", Toast.LENGTH_SHORT).show();
+                                                Intent i = new Intent(LogInActivity.this, HomeActivity.class);
+                                                startActivity(i);
+                                                finish();
+                                            } else if (type.equals("Recycler")) {
+                                                Toast.makeText(getApplicationContext(), "User Successfully SignedIn", Toast.LENGTH_SHORT).show();
+                                                Intent i = new Intent(LogInActivity.this, RecyclerHomeActivity.class);
+                                                startActivity(i);
+                                                finish();
+                                            } else {
+                                                Toast.makeText(getApplicationContext(),"Invalid Email or Password!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                                });
+
                             } else {
                                 Toast.makeText(getApplicationContext(),"Invalid Email or Password!", Toast.LENGTH_SHORT).show();
                             }
