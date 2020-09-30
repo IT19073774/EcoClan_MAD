b1471915bd6bfb66c111e992bed70cee8e20a6f5
SkullHack <poddalgodakaveen@gmail.com>
2020-09-30 13:04:28 +0530

    ecoCal Unit Test commit

--- app/src/test/java/com/example/ecoclan_v2/ExampleUnitTest.java
+++ app/src/test/java/com/example/ecoclan_v2/ExampleUnitTest.java
@@ -12,6 +12,65 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
 
 
+    //IT19056494
+    //Unit test for ecoCal
+    private ecoCal ECOCAL;
+
+    public void setECOCAL(){
+        ECOCAL = new ecoCal();
+    }
+
+    //Individual Calculations according to the material
+    @Test
+    public void GlassCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,0,0,0,1,0);
+        assertEquals(145.0,result,0.001);
+    }
+
+    @Test
+    public void PlasticCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,1,0,0,0,0);
+        assertEquals(100.0,result,0.001);
+    }
+
+    @Test
+    public void ClothCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,0,1,0,0,0);
+        assertEquals(120.0,result,0.001);
+    }
+
+    @Test
+    public void MetalCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,0,0,1,0,0);
+        assertEquals(200.0,result,0.001);
+    }
+
+    @Test
+    public void PaperCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,0,0,0,0,1);
+        assertEquals(60.0,result,0.001);
+    }
+
+    //If user inputs all zeros
+    @Test
+    public void AllZeroCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,0,0,0,0,0);
+        assertEquals(00.0,result,0.001);
+    }
+
+    //If user inputs multiple inputs
+    @Test
+    public void MultipleInputsCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,10,1,32,21,2);
+        assertEquals(10685.0,result,0.001);
+    }
+
+    //If user inputs multiple inputs
+    @Test
+    public void DecimalsInputsCalculation_isCorrect(){
+        double result = ecoCal.ecoCal_sum(100,120,200,145,60,0.2,10.2,2.8,0,2.8);
+        assertEquals(1971.99,result,0.01);
+    }
 
 
 }
